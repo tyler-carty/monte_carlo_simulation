4153af142cf2091c8ae2dbe06f0af21a653ce2fd
Tyler Cartwright <tcartwright2606@gmail.com>
2023-08-15 23:47:54 +0100

    pre-process the data and output, MD file for understanding

---
+++ src/preprocessing/preprocessing.py
@@ -0,0 +1,34 @@
+import yfinance as yf
+
+# Define the tickers of your assets
+assets = ["AAPL", "MSFT", "GOOGL", "AMZN", "TSLA", "FB", "NFLX", "JPM", "PG", "JNJ", "V", "KO", "BA", "INTC", "DIS", "GS", "NKE", "XOM", "WMT", "IBM"]
+
+# Fetch data
+data = yf.download(assets, start="2010-01-01", end="2023-01-01", group_by='ticker')
+
+# This will give you Open, High, Low, Close, Volume, and Dividends & Stock Splits data for each asset.
+
+# Filtering Relevant Data
+# For most quantitative analyses, you'll be interested in adjusted closing prices, which take into account dividends and stock splits.
+adj_close = data.loc[:, (slice(None), 'Adj Close')].copy()
+adj_close.columns = adj_close.columns.droplevel(1)
+
+# Handling Missing Data
+# Ensure that there are no missing values in your dataset. If there are, decide on a strategy to address them (e.g., forward fill, backward fill, interpolation, or dropping them).
+adj_close = adj_close.fillna(method='ffill')  # Forward fill
+
+# Calculate daily returns
+# Returns provide insights into the relative change in prices, which are pivotal for risk assessments and simulations.
+daily_returns = adj_close.pct_change().dropna()
+
+# Data Visualization:
+# It's often helpful to visualize your data to understand trends and anomalies.
+
+import matplotlib.pyplot as plt
+
+# Plotting the daily returns of AAPL as an example
+daily_returns['AAPL'].plot(figsize=(10, 6))
+plt.title('AAPL Daily Returns')
+plt.show()
+
+daily_returns.to_csv('daily_returns.csv')
---
+++ src/preprocessing/stock_reference.md
@@ -0,0 +1,28 @@
+# Stock Tickers and Their Companies
+
+This document lists popular stock tickers and their corresponding company names for easy reference.
+
+| Ticker | Company Name               |
+|--------|----------------------------|
+| AAPL   | Apple Inc.                 |
+| MSFT   | Microsoft Corporation      |
+| GOOGL  | Alphabet Inc. (Google)     |
+| AMZN   | Amazon.com, Inc.           |
+| TSLA   | Tesla, Inc.                |
+| FB     | Meta Platforms, Inc. (Facebook) |
+| NFLX   | Netflix, Inc.              |
+| JPM    | JPMorgan Chase & Co.       |
+| PG     | Procter & Gamble Co.       |
+| JNJ    | Johnson & Johnson          |
+| V      | Visa Inc.                  |
+| KO     | The Coca-Cola Co.          |
+| BA     | The Boeing Company         |
+| INTC   | Intel Corporation          |
+| DIS    | The Walt Disney Company    |
+| GS     | The Goldman Sachs Group, Inc. |
+| NKE    | NIKE, Inc.                 |
+| XOM    | Exxon Mobil Corporation    |
+| WMT    | Walmart Inc.               |
+| IBM    | International Business Machines Corporation |
+
+Note: Stock ticker symbols and company names are subject to change based on company decisions and market conditions. Always ensure accuracy by cross-referencing with official stock exchanges or company communications.
